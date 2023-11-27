e831449c9a6d6506d9b29b49e160e6858f5c046a
Akay <akaay1@icloud.com>
2023-11-27 01:22:34 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+my_name = "Akay Akbaba"
+
+course_name = "Python Programming 2023FA-CIS-024C-102 "
+curr_month = 9
+
+print(f"Hello {my_name}! The course ends on Dec, which is {12 - curr_month} months from now.")
+print("Hope you have a great time learning this course")
+
+today = 86.7
+yesterday = 87.0
+
+avg_temp = (today + yesterday) / 2
+print(f"Average temperature is {avg_temp}")
